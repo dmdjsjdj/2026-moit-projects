a9feb435e359c87f54d6ed0cd71fb8761967c297
dmdjsjdj <jenia03s@gmail.com>
2026-09-16 15:42:36 +0900

    fix: 광고 AI 요약 스케줄 주기 수정

--- moit-v3/back/src/main/java/com/moit/advertisement/scheduler/AdvertisementScheduler.java
+++ moit-v3/back/src/main/java/com/moit/advertisement/scheduler/AdvertisementScheduler.java
@@ -60,8 +60,8 @@ public class AdvertisementScheduler {
     
     
     // 매일 오전 9시 광고기간 만료 30/14일자 발송
-//    @Scheduled(cron = "0 0 9 * * *")
-    @Scheduled(cron = "0 */5 * * * *")
+    @Scheduled(cron = "0 0 9 * * *")
+//    @Scheduled(cron = "0 */5 * * * *")
 
     public void advertisementReminder() {
     	log.info("광고 연장 안내 메일 스케줄러 실행");
@@ -73,7 +73,7 @@ public class AdvertisementScheduler {
     
     
     // 3시간마다 통계 ai 요약 저장
-    @Scheduled(cron = "0 0 */3 * * *")
+    @Scheduled(cron = "0 0 2 * * *")
     //@Scheduled(cron = "0 */1 * * * *")
 
     public void generateAiSummary() {
